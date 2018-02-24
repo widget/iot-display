ccdd4e859829165c4b98ac9922a135b0ef671ade
Widget <widget@stdin.co.uk>
2018-02-24 11:04:25 +0000

    Use logging instead of print. Try forcing updates but actually our wakeup time is screwed. Fixed waking up too late by subtracting SLACK. PEP8 fixes.

--- server/test.py
+++ server/test.py
@@ -7,6 +7,7 @@ import json
 import os.path
 import pytz
 import sys
+import logging
 
 import xml.etree.ElementTree as ET
 
@@ -17,9 +18,9 @@ from tide_parser import TideParser
 from weather import Weather
 
 
-def generate_status_page(metadata_supplied, wake_up_time):
+def generate_status_page(metadata_supplied, wake_up_time, status_path):
     try:
-        with open("status.html", "w") as statusfile:
+        with open(status_path, "w") as statusfile:
             statusfile.write("<html><head>Current status</head><body>")
             statusfile.write("<p>Next wakeup due at %s</p>" % wake_up_time.isoformat())
 
@@ -36,6 +37,10 @@ def generate_status_page(metadata_supplied, wake_up_time):
         pass
 
 
+def print_time(dt):
+    return dt.strftime("%Y-%m-%dT%H:%M:%S")
+
+
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime.datetime):
@@ -43,15 +48,18 @@ class DateTimeEncoder(json.JSONEncoder):
 
         return json.JSONEncoder.default(self, o)
 
+
 SLACK = datetime.timedelta(minutes=15)
 
 CLIENT_METADATA = "metadata.json"
 SERVER_METADATA = "server.xml"
+SERVER_STATUS = "status.html"
 OUTPUT_EPD = "data.bin"
 OUTPUT_PNG = "data.png"
 
 london = pytz.timezone("Europe/London")
 gmt = pytz.timezone("GMT")
+
 
 parser = argparse.ArgumentParser(description="Generate new files")
 parser.add_argument('-d', '--dir', default='.')
@@ -59,12 +67,18 @@ parser.add_argument('-v', '--verbose', action='store_const', const=True, default
 parser.add_argument('-t', '--time', help="In the form %%Y-%%m-%%d %%H:%%M")
 parser.add_argument('-f', '--force', help='Force a wakeup', action='store_const', const=True, default=False)
 parser.add_argument('-c', '--config', help='Configuration', required=True)
+parser.add_argument('--min', help="Ensure wake-ups at least min time from now", type=int, default=-1)
 
 args = parser.parse_args()
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG if args.verbose else logging.INFO)
 
 if not os.path.exists(args.dir):
     print("Non-existent output directory")
     sys.exit(1)
+else:
+    logging.debug("Using " + args.dir)
 
 config = configparser.ConfigParser()
 config.read(args.config)
@@ -73,6 +87,8 @@ client_metadata_path = config.get("General", "ClientMetadata",
                                   fallback=os.path.join(args.dir, CLIENT_METADATA))
 server_metadata_path = config.get("General", "ServerMetadata",
                                   fallback=os.path.join(args.dir, SERVER_METADATA))
+server_status_path = config.get("General", "ServerStatus",
+                                fallback=os.path.join(args.dir, SERVER_STATUS))
 output_epd_path = config.get("General", "DisplayOutput",
                              fallback=os.path.join(args.dir, OUTPUT_EPD))
 output_png_path = config.get("General", "DebugOutput",
@@ -88,24 +104,23 @@ else:
         current_local = datetime.datetime.strptime(args.time, "%H:%M")
         current_local = datetime.datetime.combine(datetime.date.today(),
                                                   current_local.time())
-        print("Hard-coding time to %s" % current_local)
-current_local = london.localize(current_local)
+        logging.info("Hard-coding time to %s" % current_local)
 
+current_local = london.localize(current_local)
 day_start = london.localize(datetime.datetime.combine(current_local.today(), datetime.time(0)))
 
 metadata = ET.ElementTree(ET.Element("display"))
 if os.path.exists(server_metadata_path):
     metadata = ET.parse(server_metadata_path)
 else:
-    if args.verbose:
-        print("No old metadata to load")
+    logging.debug("No old metadata to load")
 
 server_node = metadata.find("./server")
 if server_node is None:
     server_node = ET.SubElement(metadata.getroot(), "server")
 
 wakeup_node = metadata.find('server/wake')
-if wakeup_node is None: # can't use if not?
+if wakeup_node is None:  # can't use if not?
     wakeup_node = ET.SubElement(server_node, "wake")
 
 default_time = True
@@ -118,16 +133,16 @@ try:
         next_wake = london.localize(datetime.datetime.strptime(timeval, "%Y-%m-%dT%H:%M:%S"))
         default_time = False
 except ValueError:
-    print("Failed to read date from wakeup node: '%s'" % wakeup_node.attrib["time"])
+    logging.error("Failed to read date from wakeup node: '%s'" % wakeup_node.attrib["time"])
 
 if default_time:
-    print("Using default wake time")
+    logging.info("Using default wake time")
     next_wake = london.localize(datetime.datetime(year=1980, month=1, day=1))
 
 # Load tides
 tides_node = metadata.find('./server/tides')
 if not tides_node:
-    print("Creating new tides node")
+    logging.info("Creating new tides node")
     tides_node = ET.SubElement(server_node, "tides")
 
 loaded_tides = []
@@ -136,8 +151,7 @@ for node in metadata.findall('./server/tides/tide'):
              node.attrib["type"],
              float(node.attrib["height"]))
     loaded_tides.append(t)
-    if args.verbose:
-        print("Loading stored %s" % t)
+    logging.debug("Loading stored %s" % t)
 
 # Pull the battery value out
 last_log_node = metadata.findall('./client/log[last()]')
@@ -146,17 +160,15 @@ if len(last_log_node) == 1:
     battery = int(last_log_node[0].attrib["battery"])
 
 if not args.force:
-    if current_local < next_wake:
-        if args.verbose:
-            print("Waking too early (not yet %s)" % next_wake)
+    if current_local < (next_wake-SLACK):
+        logging.debug("Waking too early (not yet %s)" % (next_wake-SLACK))
         sys.exit(0)
 
 valid_tides = [tide for tide in loaded_tides if tide.time > day_start]
 tides_downloaded = []
 if len(valid_tides) < 7:
     try:
-        if args.verbose:
-            print("Fetching new tides")
+        logging.debug("Fetching new tides")
 
         # TODO error handle here
         feed_loc = config["Tides"]["Feed"]
@@ -165,7 +177,7 @@ if len(valid_tides) < 7:
         t = TideParser(feed_loc)
         tides_downloaded = t.fetch(args.verbose)
     except ConnectionError:
-        print("Failed to fetch tides")
+        logging.error("Failed to fetch tides")
 else:
     # print("Using cached data")
     tides_downloaded = loaded_tides
@@ -174,9 +186,9 @@ location = (config.getfloat("Geo", "Latitude"),
             config.getfloat("Geo", "Longitude"))
 
 if args.verbose:
-    print("Tide times:")
+    logging.debug("Tide times:")
     for t in tides_downloaded:
-        print(t)
+        logging.debug(t)
 
 if len(tides_downloaded) == 0:
     # TODO handle error
@@ -186,12 +198,13 @@ future_tides = [tide for tide in tides_downloaded if tide.time > current_local]
 today_tides = [tide for tide in tides_downloaded if tide.time > day_start]
 
 if args.verbose:
-    print("Future tide times:")
+    logging.debug("Future tide times:")
     for t in future_tides:
-        print(t)
+        logging.debug(t)
 
 # Wakeup into tomorrow, although the RSS feed is a bit slower
-wake_up_time_gmt = datetime.datetime.combine(current_local.date() + datetime.timedelta(days=1), datetime.time(hour=1, minute=15))
+wake_up_time_gmt = datetime.datetime.combine(current_local.date() + datetime.timedelta(days=1),
+                                             datetime.time(hour=1, minute=15))
 wake_up_time_gmt = gmt.localize(wake_up_time_gmt)
 
 try:
@@ -201,38 +214,43 @@ try:
 except configparser.NoOptionError as e:
     weather = None
 except Exception as e:
-    print("Failed to fetch weather information: ")
-    print(str(type(e)))
-    print(e)
+    logging.exception("Failed to fetch weather information")
     weather = None
 
 d = None
 if len(future_tides) >= 2:
     # Otherwise wakeup when we need to change the clock
-    wake_up_time_gmt = future_tides[0].time # GMT! not astimezone(london)
-    d = DisplayRenderer(future_tides[0], future_tides[1], battery=battery, location=location, weather=weather, tz=london)
+    wake_up_time_gmt = future_tides[0].time  # GMT! not astimezone(london)
+    d = DisplayRenderer(future_tides[0], future_tides[1],
+                        battery=battery, location=location, weather=weather, tz=london)
 elif len(future_tides) == 1:
-    d = DisplayRenderer(future_tides[0], battery=battery, location=location, weather=weather, tz=london)
-    wake_up_time_gmt = future_tides[0].time # GMT! not astimezone(london)
+    d = DisplayRenderer(future_tides[0],
+                        battery=battery, location=location, weather=weather, tz=london)
+    wake_up_time_gmt = future_tides[0].time  # GMT! not astimezone(london)
 else:
     # For the sake of something to show we show this morning's, as next morning
-    d = DisplayRenderer(today_tides[0], battery=battery, location=location, weather=weather)
-
-# Remove microseconds
-wake_up_time_gmt = wake_up_time_gmt.replace(microsecond=0)
+    d = DisplayRenderer(today_tides[0],
+                        battery=battery, location=location, weather=weather)
 
 tides_node.clear()
 
 for tide in tides_downloaded:
     tides_node.append(tide.to_xml())
 
-# OVERRIDE TO NO MORE THAN FOUR HOURS
-max_sleep = datetime.timedelta(hours=4)
-if wake_up_time_gmt > (current_local + max_sleep):
-    wake_up_time_gmt = current_local + max_sleep
+# if args.min != -1:
+#     # OVERRIDE TO NO MORE THAN FOUR HOURS
+#     max_sleep = datetime.timedelta(minutes=args.min)
+#     if wake_up_time_gmt > (current_local + max_sleep):
+#         logging.info("Original wakeup of %s" % print_time(wake_up_time_gmt))
+#
+#         wake_up_time_gmt = current_local + max_sleep
+#         logging.info("Now %s (%d min)" % (print_time(wake_up_time_gmt), args.min))
 
 # Now the time for the client to wakeup
 wake_up_time_gmt += SLACK
+
+# Remove microseconds
+wake_up_time_gmt = wake_up_time_gmt.replace(microsecond=0)
 
 # isoformat puts out tz info in the wrong format to be able to bloody load it again
 wut = wake_up_time_gmt.isoformat().split('+')[0]
@@ -243,14 +261,12 @@ client_node = metadata.find('./client')
 
 if client_node:
     if not metadata.findall("./client/requested[@time='%s']" % wut):
-        wakeup_log = ET.SubElement(client_node, "requested")
+        wakeup_log = ET.SubElement(client_node, "requested")  # as in when the client should have come in
         wakeup_log.attrib["time"] = wut
 
 metadata.write(server_metadata_path, xml_declaration=True)
 
-if args.verbose:
-    # We'll only get log messages dated when we did it and when the next one should be
-    print("Next client wakeup is %s" % wake_up_time_gmt)
+logging.debug("Next client wakeup is %s" % wake_up_time_gmt)
 
 client_metadata = {"wakeup": wake_up_time_gmt.timetuple()}
 
@@ -266,6 +282,6 @@ if d:
     e.save(output_epd_path)
 
 else:
-    print("Skipping render, no RSS data")
+    logging.info("Skipping render, no RSS data")
 
-generate_status_page(metadata, wake_up_time_gmt)
+generate_status_page(metadata, wake_up_time_gmt, server_status_path)
