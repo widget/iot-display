c5f1e128b46b1baeeed750565b483f36d8480c3e
Widget <widget@stdin.co.uk>
2020-12-22 15:02:48 +0000

    fix: pre-commit file fixings

---
+++ server/comms/app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, abort
+from lxml import etree
+from datetime import datetime
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+app = Flask(__name__, static_folder="/static")
+
+SERVER_METADATA = "/static/server.xml"
+MAX_ENTRIES = 1000
+
+
+@app.route('/upload.php', methods=['POST'])
+def hello_world():
+    battery = request.form.get("battery")
+    reset = request.form.get("reset")
+    screen = request.form.get("screen")
+    if not battery:
+        app.logger.info("No battery key in POST")
+        abort(400)
+
+    if not reset:
+        app.logger.info("No reset key in POST")
+        abort(400)
+
+    if not screen:
+        app.logger.info("No screen key in POST")
+        abort(400)
+
+    # Get the base XML
+    try:
+        parser = etree.XMLParser(remove_blank_text=True)
+        metadata = etree.parse(SERVER_METADATA, parser)
+        app.logger.debug("Loaded existing XML")
+    except OSError:
+        app.logger.exception("Can't open XML file")
+        metadata = etree.ElementTree(ET.Element("display"))
+
+    # Get the client node
+    client_node = metadata.find("./client")
+    if client_node is None:
+        client_node = etree.SubElement(metadata.getroot(), "client")
+
+    # Delete node if there's too many
+    num_children = len(client_node)
+    app.logger.info("Currently %d entries" % num_children)
+    if num_children > MAX_ENTRIES:
+        to_delete = num_children - MAX_ENTRIES
+        app.logger.info("Deleting oldest %d", to_delete)
+        for _ in range(to_delete):
+            client_node.remove(client_node.getchildren()[0])
+
+    # Store current values
+    latest = etree.SubElement(client_node, "log")
+    latest.attrib["battery"] = battery
+    latest.attrib["reset"] = reset
+    latest.attrib["screen"] = screen
+    latest.attrib["ip"] = request.remote_addr
+    latest.attrib["time"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+    metadata.write(SERVER_METADATA, xml_declaration=True, pretty_print=True)
+    return ""
+
+
+@app.route('/data.bin')
+def data_bin():
+    app.logger.info("Binary image requested")
+    return app.send_static_file('data.bin')
+
+
+@app.route('/data.png')
+def data_png():
+    app.logger.info("Preview requested")
+    return app.send_static_file('data.png')
+
+
+@app.route('/status.html')
+def send_status():
+    app.logger.info("Status page")
+    return app.send_static_file('status.html')
+
+
+@app.route('/metadata.json')
+def send_metadata():
+    app.logger.info("Metadata")
+    return app.send_static_file('metadata.json')
---
+++ server/updater/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+while true
+do
+  python tideclock_generator.py -c /data/mine.cfg
+  sleep 600
+done
